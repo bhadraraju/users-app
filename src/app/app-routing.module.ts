fcdc52c82dc263c45e1a64bcc17652af5c074c83
Chandra Mouli Bhadraraju <c.a.mouli@accenture.com>
2020-06-23 16:42:15 +0100

    news route added for aditya

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { UserlistComponent } from './userlist/userlist.component';
+import {NewsComponent} from "./news/news.component";
 
 
 const routes: Routes = [
   { path: '',   redirectTo: 'userlist', pathMatch: 'full' },
   { path: 'user', component: UserComponent },
-  { path: 'userlist', component: UserlistComponent }
+  { path: 'userlist', component: UserlistComponent },
+  { path: 'news', component: NewsComponent }
+
 ];
 
 @NgModule({
